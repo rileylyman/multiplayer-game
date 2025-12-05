a55b8d231f4a7b255a01e0b05e5d844a04358684
Riley Lyman <rileylyman@berkeley.edu>
2025-12-05 14:31:01 -0500

    basic tennis

---
+++ scripts/tennis_ball.gd
@@ -0,0 +1,48 @@
+class_name TennisBall extends Area2D
+
+@export var speed: float = 400.0
+@export var max_height: float = 100.0
+
+@onready var sprite: Sprite2D = $Sprite
+@onready var target_tracker: Sprite2D = $"../TargetTracker"
+
+var _start: Vector2
+var _target: Vector2
+var _duration: float
+var _elapsed: float
+
+func _ready() -> void:
+    global_position = Vector2(500, 0)
+    hit_towards(Vector2(-500, 0))
+
+    body_entered.connect(func(body):
+        if body is TennisPlayer:
+            body.ball = self
+    )
+
+    body_exited.connect(func(body):
+        if body is TennisPlayer:
+            body.ball = null
+    )
+
+func hit_towards(target: Vector2) -> void:
+    _target = target
+    _start = global_position
+    _duration = max((_target - _start).length() / speed, 1.5)
+    _elapsed = 0.0
+
+    target_tracker.global_position = _target
+
+func is_near_ground() -> bool:
+    return _elapsed / _duration > 0.75
+
+func _process(delta: float) -> void:
+    _elapsed += delta
+
+    var t = _elapsed / _duration * 2.0 - 1.0
+    global_position = _start.lerp(_target, (t + 1.0) / 2.0)
+    if t > 1.1:
+        _ready()
+
+    var height = max_height * (1 - t * t)
+    sprite.position.y = - height
---
+++ scripts/tennis_player.gd
@@ -0,0 +1,39 @@
+class_name TennisPlayer extends CharacterBody2D
+
+@export var player := Utils.PlayerType.PLAYER_1
+@export var speed := 300.0
+
+@onready var other_player: TennisPlayer = $"/root/Tennis/TennisPlayer2" if player == Utils.PlayerType.PLAYER_1 else $"/root/Tennis/TennisPlayer"
+@onready var sprite: Sprite2D = $Sprite2D
+
+var ball: TennisBall = null
+
+func _get_hit_to_point() -> Vector2:
+    var wssr = Utils.wssr()
+    var buffer := 200.0
+    # var x
+    # if global_position.x < 0:
+    #     x = randf_range(buffer, wssr.end.x - buffer)
+    # else:
+    #     x = randf_range(wssr.position.x + buffer, -buffer)
+    var x = randf_range(other_player.global_position.x - buffer, other_player.global_position.x + buffer)
+    var y = randf_range(other_player.global_position.y - buffer, other_player.global_position.y + buffer)
+    var ret = Vector2(x, y)
+    return ret.clamp(wssr.position, wssr.end)
+
+
+func _physics_process(_delta: float) -> void:
+    var move_vector = Utils.get_player_move_vector("move_left", "move_right", "move_up", "move_down", player)
+    var desired = speed * move_vector
+    velocity = velocity.lerp(desired, 0.1)
+    move_and_slide()
+    _clamp_pos_to_screen()
+
+    if ball != null and ball.is_near_ground():
+        if Utils.is_player_action_just_pressed("button1", player) or Utils.is_player_action_just_pressed("button2", player):
+            ball.hit_towards(_get_hit_to_point())
+
+func _clamp_pos_to_screen() -> void:
+    var screen_rect = Utils.get_global_viewport_rect()
+    var sprite_rect = Utils.get_sprite_global_rect(sprite)
+    global_position = global_position.clamp(screen_rect.position + sprite_rect.size / 2, screen_rect.end - sprite_rect.size / 2)
