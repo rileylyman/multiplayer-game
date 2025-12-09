91a29bca47eb7a85ec25733614dd0ec395af23b7
lw3100-alt <lw3100@nyu.edu>
2025-12-09 01:03:04 -0500

    tennis animation

--- scripts/tennis_ball.gd
+++ scripts/tennis_ball.gd
@@ -12,43 +12,43 @@ var _duration: float
 var _elapsed: float
 
 func _ready() -> void:
-    on_ground_fall()
-    body_entered.connect(func(body):
-        if body is TennisPlayer:
-            body.ball = self
-    )
+	on_ground_fall()
+	body_entered.connect(func(body):
+		if body is TennisPlayer:
+			body.ball = self
+	)
 
-    body_exited.connect(func(body):
-        if body is TennisPlayer:
-            body.ball = null
-    )
+	body_exited.connect(func(body):
+		if body is TennisPlayer:
+			body.ball = null
+	)
 
 func on_ground_fall() -> void:
-    if position.x > 0:
-        global_position = Vector2(500, 0)
-        hit_towards(Vector2(-500, 0))
-    else:
-        global_position = Vector2(-500, 0)
-        hit_towards(Vector2(500, 0))
+	if position.x > 0:
+		global_position = Vector2(500, 0)
+		hit_towards(Vector2(-500, 0))
+	else:
+		global_position = Vector2(-500, 0)
+		hit_towards(Vector2(500, 0))
 
 func hit_towards(target: Vector2) -> void:
-    _target = target
-    _start = global_position
-    _duration = max((_target - _start).length() / speed, 1.5)
-    _elapsed = 0.0
+	_target = target
+	_start = global_position
+	_duration = max((_target - _start).length() / speed, 1.5)
+	_elapsed = 0.0
 
-    target_tracker.global_position = _target
+	target_tracker.global_position = _target
 
 func is_near_ground() -> bool:
-    return _elapsed / _duration > 0.75
+	return _elapsed / _duration > 0.6
 
 func _process(delta: float) -> void:
-    _elapsed += delta
+	_elapsed += delta
 
-    var t = _elapsed / _duration * 2.0 - 1.0
-    global_position = _start.lerp(_target, (t + 1.0) / 2.0)
-    if t > 1.1:
-        on_ground_fall()
+	var t = _elapsed / _duration * 2.0 - 1.0
+	global_position = _start.lerp(_target, (t + 1.0) / 2.0)
+	if t > 1.1:
+		on_ground_fall()
 
-    var height = max_height * (1 - t * t)
-    sprite.position.y = - height
+	var height = max_height * (1 - t * t)
+	sprite.position.y = - height
--- scripts/tennis_player.gd
+++ scripts/tennis_player.gd
@@ -5,36 +5,111 @@ class_name TennisPlayer extends CharacterBody2D
 
 @onready var other_player: TennisPlayer = $"/root/SceneRoot/TennisPlayer2" if player == Utils.PlayerType.PLAYER_1 else $"/root/SceneRoot/TennisPlayer"
 @onready var sprite: Sprite2D = $Sprite2D
+@onready var visual_p1: Node2D = $VisualP1
+@onready var visual_p2: Node2D = $VisualP2
+
+var front: AnimatedSprite2D
+var mid: AnimatedSprite2D
+var back: AnimatedSprite2D
+var ap: AnimationPlayer
+
+var move_state := "idle"
+var mid_is_hitting := false
 
 var ball: TennisBall = null
 
-func _get_hit_to_point() -> Vector2:
-    var wssr = Utils.wssr()
-    var buffer := 200.0
-    # var x
-    # if global_position.x < 0:
-    #     x = randf_range(buffer, wssr.end.x - buffer)
-    # else:
-    #     x = randf_range(wssr.position.x + buffer, -buffer)
-    var x = randf_range(other_player.global_position.x - buffer, other_player.global_position.x + buffer)
-    var y = randf_range(other_player.global_position.y - buffer, other_player.global_position.y + buffer)
-    var ret = Vector2(x, y)
-    return ret.clamp(wssr.position, wssr.end)
+func _ready() -> void:
+	if player == Utils.PlayerType.PLAYER_1:
+		visual_p1.visible = true
+		visual_p2.visible = false
+		front = visual_p1.get_node("front")
+		mid = visual_p1.get_node("mid")
+		back = visual_p1.get_node("back")
+		ap = visual_p1.get_node("AnimationPlayer")
+	else:
+		visual_p1.visible = false
+		visual_p2.visible = true
+		front = visual_p2.get_node("front")
+		mid = visual_p2.get_node("mid")
+		back = visual_p2.get_node("back")
+		ap = visual_p2.get_node("AnimationPlayer")
 
+	mid.animation_finished.connect(_on_mid_animation_finished)
+
+	front.play("idle")
+	mid.play("idle")
+	back.play("idle")
+	ap.play("RESET")
+	
+func _get_hit_to_point() -> Vector2:
+	var wssr = Utils.wssr()
+	var buffer := 200.0
+	# var x
+	# if global_position.x < 0:
+	#     x = randf_range(buffer, wssr.end.x - buffer)
+	# else:
+	#     x = randf_range(wssr.position.x + buffer, -buffer)
+	var x = randf_range(other_player.global_position.x - buffer, other_player.global_position.x + buffer)
+	var y = randf_range(other_player.global_position.y - buffer, other_player.global_position.y + buffer)
+	var ret = Vector2(x, y)
+	var margin := 50.0
+	var p1 = wssr.position + Vector2(margin, margin)
+	var p2 = wssr.end - Vector2(margin, margin)
+	return ret.clamp(p1, p2)
+	
+func set_move_state(state: String) -> void:
+	if state == move_state:
+		return
+	move_state = state
+	if state == "idle":
+		front.play("idle")
+		back.play("idle")
+		if not mid_is_hitting:
+			mid.play("idle")
+		ap.play("RESET")
+	elif state == "run":
+		front.play("run")
+		back.play("run")
+		if not mid_is_hitting:
+			mid.play("run")
+		ap.play("run_motion")
+
+func play_hit() -> void:
+	mid_is_hitting = true
+	mid.stop()
+	mid.frame = 0
+	mid.play("hit")
+
+func _on_mid_animation_finished() -> void:
+	if mid.animation == "hit":
+		mid_is_hitting = false
+		if move_state == "run":
+			mid.play("run")
+		else:
+			mid.play("idle")
 
 func _physics_process(_delta: float) -> void:
-    var move_vector = Utils.get_player_move_vector("move_left", "move_right", "move_up", "move_down", player)
-    var desired = speed * move_vector
-    velocity = velocity.lerp(desired, 0.1)
-    move_and_slide()
-    _clamp_pos_to_screen()
+	var move_vector = Utils.get_player_move_vector("move_left", "move_right", "move_up", "move_down", player)
+	var desired = speed * move_vector
+	velocity = velocity.lerp(desired, 0.1)
+	move_and_slide()
+	_clamp_pos_to_screen()
+	var new_state := move_state
+	if move_vector.length() > 0.1:
+		new_state = "run"
+	else:
+		new_state = "idle"
+	if new_state != move_state:
+		set_move_state(new_state)
 
-    if ball != null and ball.is_near_ground():
-        if Utils.is_player_action_just_pressed("button1", player):
-            ball.hit_towards(_get_hit_to_point())
-            GameManager.add_score(19, player)
+	if Utils.is_player_action_just_pressed("button1", player):
+		play_hit()
+		if ball != null and ball.is_near_ground():
+			ball.global_position = global_position
+			ball.hit_towards(_get_hit_to_point())
+			GameManager.add_score(19, player)
 
 func _clamp_pos_to_screen() -> void:
-    var screen_rect = Utils.get_global_viewport_rect()
-    var sprite_rect = Utils.get_sprite_global_rect(sprite)
-    global_position = global_position.clamp(screen_rect.position + sprite_rect.size / 2, screen_rect.end - sprite_rect.size / 2)
+	var screen_rect = Utils.get_global_viewport_rect()
+	var sprite_rect = Utils.get_sprite_global_rect(sprite)
+	global_position = global_position.clamp(screen_rect.position + sprite_rect.size / 2, screen_rect.end - sprite_rect.size / 2)
