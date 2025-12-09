379b7411c42bb62d39d49b6e29835426cc5b6110
Riley Lyman <rileylyman@berkeley.edu>
2025-12-08 22:41:13 -0500

    add crowns

--- scripts/transition_animation.gd
+++ scripts/transition_animation.gd
@@ -3,10 +3,20 @@ extends AnimationPlayer
 @export var instruction: Label
 @export var score: Label
 
+@export var left_crown: Sprite2D
+@export var right_crown: Sprite2D
+
 func _ready() -> void:
     instruction.text = GameManager.get_transition_instruction_text()
     var needs_score_tick: bool = GameManager.score_adds[0] > 0 or GameManager.score_adds[1] > 0
     score.adds_visible(needs_score_tick)
+
+    if not GameManager.is_last_scene():
+        left_crown.visible = GameManager.score_adds[0] > GameManager.score_adds[1]
+        right_crown.visible = GameManager.score_adds[1] > GameManager.score_adds[0]
+    else:
+        left_crown.visible = GameManager.score_adds[0] + GameManager.player_scores[0] >= GameManager.score_adds[1] + GameManager.player_scores[1]
+        right_crown.visible = GameManager.score_adds[1] + GameManager.player_scores[1] >= GameManager.score_adds[0] + GameManager.player_scores[0]
 
     GameManager.start_scene()
     play("up")
